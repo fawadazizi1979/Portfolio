7f657ebf0203c2ecbe6416cb63afc65d7132943f
Ahmad Fawad Azizi <137276812+fawadazizi1979@users.noreply.github.com>
2025-03-24 18:12:38 +0430

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -5,3 +5,17 @@ var typed= new Typed(".text",{
     backdelay:1000,
     loop:true
 })
+document.querySelectorAll('.navbar a').forEach(link => {
+    link.addEventListener('click', function(e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+        
+        if (targetElement) {
+            window.scrollTo({
+                top: targetElement.offsetTop - 50, // تنظیم فاصله از بالا
+                behavior: 'smooth'
+            });
+        }
+    });
+});
